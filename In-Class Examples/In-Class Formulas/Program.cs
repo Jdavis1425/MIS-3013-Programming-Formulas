bfe6a3604bcb9d3965f95d0e01aeb0f2d71f6c80
Jatin Davis <jatinmdavis@ou.edu>
2020-01-22 12:54:11 -0600

    Created variables for firstName, lastName, favoriteNumber, also derived crazyNumber from it

--- In-Class Examples/In-Class Formulas/Program.cs
+++ In-Class Examples/In-Class Formulas/Program.cs
@@ -11,6 +11,25 @@ namespace In_Class_Formulas
     {
         static void Main(string[] args)
         {
+            // create a string named firstName
+            string firstName;
+
+            //Assign a value to firstName 
+            firstName = "Jatin";
+
+            //Create an int named favoriteNumber and assign your favorite number
+            int favoriteNumber = 4;
+
+            double crazyNumber = 7.592045 * favoriteNumber;
+
+            string lastName = "Davis";
+            string fullName = firstName + " " + lastName;
+
+            Console.WriteLine(fullName + " has favorite number of " + favoriteNumber + " \nwhich equates to a crazy number of " + crazyNumber);
+
+            //MAKE IT DYNAMIC 
+
+            Console.ReadKey();
         }
     }
 }
