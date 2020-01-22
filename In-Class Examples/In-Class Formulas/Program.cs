7af15225976a402496f7da74c3ec36d38298710b
Jatin Davis <jatinmdavis@ou.edu>
2020-01-22 12:34:29 -0600

    Created the in class examples solution and created a project for variables

---
+++ In-Class Examples/In-Class Formulas/Program.cs
@@ -0,0 +1,16 @@
+﻿//Jatin Davis
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace In_Class_Formulas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
