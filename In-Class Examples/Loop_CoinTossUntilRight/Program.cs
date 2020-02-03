59dc9941b2d10ff18833877b1a9b4081104f774e
Jatin Davis <jatinmdavis@ou.edu>
2020-02-03 13:06:05 -0600

    Loops

---
+++ In-Class Examples/Loop_CoinTossUntilRight/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+                       
+            
+            //commandds an input from the user
+            Console.WriteLine("Enter Heads or Tails >>");
+            string usersGuessAsString = Console.ReadLine();
+            int usersGuess;
+            //declare our constant variables
+            const int HEADS_VALUE = 0;
+            const int TAILS_VALUE = 1;
+            //conditional statement to see what the value of a flip would be
+            if (usersGuessAsString == "Heads")
+            {
+                usersGuess = HEADS_VALUE;
+            }
+            else if (usersGuessAsString == "Tails")
+            {
+                usersGuess = TAILS_VALUE;
+            }
+            else
+            {
+                Console.WriteLine("Invalid entry: Please try again later");
+                Console.ReadKey();
+                return;
+            }
+            //generates a random number from the flip
+            Random rand = new Random();
+
+            //use the next method on 'rand' and pass in 0,2 to generate a random number that is 0 or 1
+            int randomNumber = rand.Next(0, 2);
+            //conditional statement to to show result of flip and whether guess is correct
+            if (usersGuess == randomNumber)
+            {
+                Console.WriteLine($"Congrats, {usersGuessAsString} was right!");
+            }
+            else
+            {
+                if (usersGuess == 0)
+                {
+                    Console.WriteLine($"Sorry, Tails was the correct answer and you guessed {usersGuessAsString}");
+                }
+                else
+                {
+                    Console.WriteLine($"Sorry, Heads was the correct answer and you guessed {usersGuessAsString}");
+                }
+            }
+
+            Console.ReadKey(); //Prevent the application from closing until we press a key
+
+        }
+    }
+}
---
+++ In-Class Examples/Loop_Summing/Program.cs
@@ -0,0 +1,47 @@
+﻿//Jatin Davis
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Summing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What number do you want to count to? >>");
+            //string input = console.readline();
+            //int threshold = convert.toint32(input);
+            int threshold = Convert.ToInt32(Console.ReadLine());
+
+            //could also use i<101
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            double sum = 0;
+            string answer;
+
+            do
+            {
+                Console.WriteLine("Please give us a # to add >>");
+                int number = Convert.ToInt32(Console.ReadLine());
+                //sum = sum + number;
+                sum += number;
+
+                Console.WriteLine("Do you have another # to add? (Y,N) >>");
+                answer = Console.ReadLine();
+
+
+            } while (answer == "Y");
+            //} while (answer != "N");
+            Console.WriteLine($"Your summation of values is {sum.ToString("N2")}");
+            
+
+            Console.ReadKey();
+        }
+    }
+}
