293f009452978cc29ffe6b954f2e34982f7150ea
davi2787 <jatindavis@gmail.com>
2020-02-24 13:11:39 -0600

    Exam 1 Review Question 1 completed.

---
+++ Exam1Review/DaysInTheMonth/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysInTheMonth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //define the variable we are adjusting throughout
+            string monthName = "";
+
+            //get user input, store in variable
+            Console.WriteLine("Enter the month you would like: {i.e. January, February, etc..");
+            monthName = Console.ReadLine();
+
+            //Process- Determine how many days are in the users month
+            if (monthName == "January")
+            {
+                Console.WriteLine("There are 31 days in January");
+            }
+            else if (monthName == "February")
+            {
+
+                Console.WriteLine("Is it a Leap year? {Yes or No}");
+                string leapYear = Console.ReadLine();
+
+                if (leapYear == "Yes")
+                {
+                    Console.WriteLine("There are 29 days in February with a Leap Year");
+                }
+
+                else
+                {
+                    Console.WriteLine("There are 28 days in February without a Leap Year");
+                }
+
+
+            }
+            else if (monthName == "March")
+            {
+                Console.WriteLine("There are 31 days in March");
+            }
+            else if (monthName == "April")
+            {
+                Console.WriteLine("There are 30 days in April");
+            }
+            else if (monthName == "May") 
+            {
+                Console.WriteLine("There are 30 days in May");
+            }
+            else if (monthName == "June")
+            {
+                Console.WriteLine("There are 30 days in June");
+            }
+            else if (monthName == "July")
+            {
+                Console.WriteLine("There are 31 days in July");
+            }
+            else if (monthName == "August")
+            {
+                Console.WriteLine("There are 31 days in August");
+            }
+            else if (monthName == "September")
+            {
+                Console.WriteLine("There are 30 days in September");
+            }
+            else if (monthName == "October")
+            {
+                Console.WriteLine("There are 31 days in October");
+            }
+            else if (monthName == "November")
+            {
+                Console.WriteLine("There are 30 days in November");
+            }
+            else if (monthName == "December")
+            {
+                Console.WriteLine("There are 31 days in December");
+            }
+            Console.ReadKey();
+            }
+
+        }
+    } //if (monthName== "January" || monthName == "months with 31 days)
+      //monthDays=31; Same for 30 day months
+      //Console.WriteLine($"There are {monthDays.ToString("N0")} in {monthName}")
+
---
+++ Exam1Review/ExamReviewFormulas/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * @Author: Jatin Davis
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> allDaNumbers = new List<double>();
+            string usersAnswerAsString;
+            double usersAnswerAsDouble;
+            string enterAnotherNumber;
+
+            //complex do while loop
+            do
+            {
+                Console.WriteLine("Please enter a number >>");
+                usersAnswerAsString = Console.ReadLine();
+                bool isAValidNumber;
+
+                isAValidNumber = double.TryParse(usersAnswerAsString, out usersAnswerAsDouble);
+
+                while (isAValidNumber == false)
+                {
+                    Console.WriteLine("Please enter a valid number >>");
+                    usersAnswerAsString = Console.ReadLine();
+
+                    isAValidNumber = double.TryParse(usersAnswerAsString, out usersAnswerAsDouble);
+                }
+
+                //usersAnswerAsDouble = Convert.ToDouble(usersAnswerAsString); //Before we did tryparse
+                allDaNumbers.Add(usersAnswerAsDouble);
+
+                if (usersAnswerAsString[usersAnswerAsString.Length - 1] == '!') // very last character is a ! 
+
+                    Console.WriteLine("Do you want to enter another number? >>");
+                enterAnotherNumber = Console.ReadLine();
+            } while (enterAnotherNumber.ToUpper()[0].ToString() == "Y");
+
+
+            double bankAccountBalance = 4.58;
+            string newString = usersAnswerAsString.Replace("dumb", "brilliant").Replace("brilliant", "dumb");
+            Console.WriteLine(bankAccountBalance);
+            Console.WriteLine($"Your bank account balance is {bankAccountBalance.ToString("C5")}.");
+
+            //while (enterAnotherNumber == "yes")
+            //{
+            //    Console.WriteLine("Please enter a number >>");
+            //    usersAnswerAsString = Console.ReadLine();
+            //    usersAnswerAsDouble = Convert.ToDouble(usersAnswerAsString);
+            //    allDaNumbers.Add(usersAnswerAsDouble);
+
+            //    Console.WriteLine("Do you want to enter another number? >>");
+            //    enterAnotherNumber = Console.ReadLine();
+            //}
+
+
+        }
+    }
+}
