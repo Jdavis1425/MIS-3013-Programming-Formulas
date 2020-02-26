e869e9c5f90597148683d9e0fd9789ba3e431795
Jatin <jatinmdavis@ou.edu>
2020-02-26 10:36:35 -0600

    Reworked HW 1, 2, 3, & 4 for Exam 1 review.

---
+++ ReworkingHomeworksForReview/HW3_TuitionIncrease_2/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Jatin Davis
+ * college charges 12,000 per semester
+ * will increase by 5% each year for 7 years
+ * display projected tuition amount for next 7 years
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3_TuitionIncrease_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //define variables
+            int initialTuition = 12000;
+            double tuitionPerSemester = initialTuition;
+
+            //process
+            for (int i = 1; i < 8; i++)
+            {
+                tuitionPerSemester = tuitionPerSemester + tuitionPerSemester * (5/100.0);
+                Console.WriteLine("The tuition after year {0} will be {1}.", i ,tuitionPerSemester.ToString("$#,###"));
+            }
+
+            //output - The tuition after year 1 will be $12,600.
+            Console.WriteLine("Press any key to continue..");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReworkingHomeworksForReview/HW3_TutorRandNumGenerator_2/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Jatin Davis
+ * generate 2 random integers each between 1 and 50
+ * display the numbers
+ * if user answer is correct, congratulate them.
+ * if user answer wrong, tell them sorry try again
+ */
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3_TutorRandNumGenerator_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //define variables
+            double userAnswer;
+
+            Random randNum = new Random();
+            int n1 = randNum.Next(1, 51);
+            int n2 = randNum.Next(1, 51);
+
+            int result = n1 + n2;
+            //user input
+            //process
+            do
+            {
+                Console.WriteLine($"{n1} + {n2} = ?");
+                userAnswer = Convert.ToDouble(Console.ReadLine());
+
+                if (userAnswer!=result)
+                {
+                    Console.WriteLine("Sorry, your answer is incorrect. Please try again");
+                }
+            } while (userAnswer!=result);
+
+            //output
+            Console.WriteLine($"Congratulations, {result} is the correct answer!");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
