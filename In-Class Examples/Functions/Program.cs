b63e44ceb8bb2a69538e1eb06d5bf0fa4ece09ef
Jatin Davis <jatinmdavis@ou.edu>
2020-03-04 13:03:06 -0600

    utilizing methods. get code from ackerman

---
+++ In-Class Examples/Functions/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            List<double> examGrades = new List<double>();
+            List<double> quizGrades = new List<double>();
+            List<double> homeworkGrades = new List<double>();
+            do
+            {
+                Console.WriteLine("Please enter your exam grade..");
+                string response = Console.ReadLine();
+                double grade;
+
+                while (double.TryParse(response, out grade) != true)
+                {
+                    Console.WriteLine("Sorry, you must enter a valid number, please try again..");
+                    response = Console.ReadLine();
+                }
+                examGrades.Add(grade);
+
+                Console.WriteLine("Do you have another value to enter?");
+                answer = Console.ReadLine();
+            } while (answer.ToLower() !="no");
+
+            int sum = Add(5, 5);
+
+        }
+
+        static int Add(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+
+        static double GetValidDoubleFromUser(string message)
+        {
+            Console.WriteLine(message);
+            string response = Console.ReadLine();
+            double grade = 0;
+
+            while (double.TryParse(response, out grade) != true)
+            {
+                Console.WriteLine("Sorry, you must enter a valid number, please try again..");
+                response = Console.ReadLine();
+            }
+
+            return grade;
+        }
+    }
+}
