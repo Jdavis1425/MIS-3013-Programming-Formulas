f81579a52a084ea5d31577a5ac6e31fee480e09f
Jatin <jatinmdavis@ou.edu>
2020-04-01 13:24:28 -0500

    Classes participation building rectangle and circle. did not finish

---
+++ Participation - Rectangle and Circle Classes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation___Rectangle_and_Circle_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double w, l;
+
+            w = 7;
+            l = 8;
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Participation - Rectangle and Circle Classes/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Rectangle
+{
+	public double Length { get; set; }
+	public double Width { get; set; }
+
+	public Rectangle()
+	{
+		Length = 0;
+		Width = 0;
+	}
+
+	public double CalculateArea()
+	{
+		double Area = Width * Length;
+		return Area;
+
+	}
+	public double CalculatePerimeter()
+	{
+		double Perimeter = 2 * (Length + Width);
+		return Perimeter;
+	}
+}	
