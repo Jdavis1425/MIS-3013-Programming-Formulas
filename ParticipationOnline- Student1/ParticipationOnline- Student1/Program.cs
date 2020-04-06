ba1ebec436ee220e2d18b0865dabee04160b771a
Jatin <jatinmdavis@ou.edu>
2020-04-06 14:34:47 -0500

    Student1 participation on Classes

--- ParticipationOnline- Student1/ParticipationOnline- Student1/Program.cs
+++ ParticipationOnline- Student1/ParticipationOnline- Student1/Program.cs
@@ -10,6 +10,18 @@ namespace ParticipationOnline__Student1
     {
         static void Main(string[] args)
         {
+            Student stud1 = new Student();
+            stud1.FirstName = "Jatin";
+            stud1.LastName = "Davis";
+            stud1.CourseGrade.Add("A");
+            stud1.CourseName.Add("MIS-3013");
+            stud1.CourseGrade.Add("B");
+            stud1.CourseName.Add("MIS-3313");
+            stud1.CalculateGPA();
+
+
+
+            Console.ReadKey();
         }
     }
 }
--- ParticipationOnline- Student1/ParticipationOnline- Student1/Student.cs
+++ ParticipationOnline- Student1/ParticipationOnline- Student1/Student.cs
@@ -8,5 +8,62 @@ namespace ParticipationOnline__Student1
 {
     class Student
     {
-    }
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public List<string> CourseName { get; set; }
+
+		public List<string> CourseGrade { get; set; }
+
+		public Student()
+		{
+			FirstName = "";
+			LastName = "";
+			List<string> CourseName = new List<string>();
+			List<string> CourseGrade = new List<string>();
+			
+		}
+		public Student(string first, string second)
+		{
+			first = FirstName;
+			second = LastName;
+
+
+		}
+		public double CalculateGPA()
+		{
+			double totalPoints = 0;
+			double points = 0;
+			foreach (var item in CourseGrade)
+			{
+				if (item == "A")
+				{
+					points = 4 * 3;
+				}
+				else if (item == "B")
+				{
+					points = 3 * 3;
+				}
+				else if (item == "C")
+				{
+					points = 2 * 3;
+				}
+				else if (item == "D")
+				{
+					points = 1 * 3;
+				}
+				else
+				{
+					points = 0;
+				}
+				totalPoints += points;
+			}
+			return totalPoints / CourseGrade.Count;
+
+
+
+		}
+
+	}
 }
