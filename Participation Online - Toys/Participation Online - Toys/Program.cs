2b4ab01fedd137e68537d82e6c2a5d540d8a8ea4
Jatin <jatinmdavis@ou.edu>
2020-04-08 13:04:22 -0500

    Participation for Toy Box.

---
+++ Participation Online - Toys/Participation Online - Toys/Program.cs
@@ -0,0 +1,28 @@
+﻿//Jatin Davis
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation_Online___Toys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Toy toy1 = new Toy();
+            ToyBox tb = new ToyBox();
+
+            toy1.Manufacturer = "Hasbro";
+            toy1.Name = "Board Game";
+            toy1.Price = 10;
+            tb.Toys = new List<Toy>();
+            tb.Toys.Add("Doll");
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Participation Online - Toys/Participation Online - Toys/Toy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation_Online___Toys
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        private string Aisle { get; set; }
+
+        public Toy()
+        {
+            Manufacturer = "";
+            Name = "";
+            Price = 0;
+            Aisle = "";
+        }
+        public string GetAisle()
+        {
+            string aisleNumber;
+            Random rand = new Random();
+            aisleNumber = ($"{Manufacturer[0] + rand.Next(1, 24)}");
+            return aisleNumber;
+        }
+    }
+}
